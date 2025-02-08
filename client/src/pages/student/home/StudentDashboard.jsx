417dc95d638a161cca5f217e914af155c61f4ebd
Niyati_Patel <130044323+niyatiit@users.noreply.github.com>
2025-02-08 14:59:20 +0530

    updated student dashbord

--- client/src/pages/student/home/StudentDashboard.jsx
+++ client/src/pages/student/home/StudentDashboard.jsx
@@ -1,116 +1,114 @@
 import React from "react";
 import Dashboard from "./Dashboard";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import MyCourses from "./MyCourses";
-import logo from "../../../assets/logo.png";
+import StudentProfile from "./StudentProfile";
+import EnrolledCourses from "./EnrolledCourses";
 
 const StudentDashboard = () => {
   return (
     <>
       {/* <!-- sidebar --> */}
-     <div className=" flex gap-20 p-44 justify-center ">
+      <div className=" flex gap-20 p-44 justify-center ">
+        <div class="col-xl-3 col-lg-3 theiaStickySidebar">
+          <div class="settings-widget dash-profile">
+            <div class="settings-menu">
+              <div class="profile-bg">
+                <div class="profile-img">
+                  <a href="student-profile.html">
+                    <img src="assets/img/user/user16.jpg" alt="Img" />
+                  </a>
+                </div>
+              </div>
+              <div class="profile-group">
+                <div class="profile-name text-center">
+                  <h4>
+                    <a href="student-profile.html">Rolands Richard</a>
+                  </h4>
+                  <p>Student</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="settings-widget account-settings">
+            <div className="settings-menu">
+              <h3>Dashboard</h3>
+              <ul>
+                <li className="nav-item active">
+                  <Link to="/student-dashboard" className="nav-link">
+                    <i className="bx bxs-tachometer"></i>Dashboard
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student/student-profile" className="nav-link">
+                    <i className="bx bxs-user"></i>My Profile
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student/student-courses" className="nav-link">
+                    <i className="bx bxs-graduation"></i>Enrolled Courses
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student-quiz" className="nav-link">
+                    <i className="bx bxs-shapes"></i>My Quiz Attempts
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student-order-history" className="nav-link">
+                    <i className="bx bxs-cart"></i>Order History
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student-qa" className="nav-link">
+                    <i className="bx bxs-bookmark-alt"></i>Question & Answer
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student-referral" className="nav-link">
+                    <i className="bx bxs-user-plus"></i>Referrals
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student-messages" className="nav-link">
+                    <i className="bx bxs-chat"></i>Messages
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/student-tickets" className="nav-link">
+                    <i className="bx bxs-coupon"></i>Support Tickets
+                  </Link>
+                </li>
+              </ul>
+              <h3>Account Settings</h3>
+              <ul>
+                <li className="nav-item">
+                  <Link to="/student-settings" className="nav-link">
+                    <i className="bx bxs-cog"></i>Settings
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    <i className="bx bxs-log-out"></i>Logout
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+        <Routes>
+          <Route path={"/"} element={<Dashboard/>}/>
+          <Route path={"student-profile"} element={<StudentProfile/>}/>
+          <Route path={"student-courses"} element={<EnrolledCourses/>}/>
+          
+        </Routes>
+      </div>
+        {/* <!-- /Sidebar --> */}
+      </div>
 
- 
-      <div class="col-xl-3 col-lg-3 theiaStickySidebar">
-        <div class="settings-widget dash-profile">
-          <div class="settings-menu">
-            <div class="profile-bg">
-              <div class="profile-img">
-                <a href="student-profile.html">
-                  <img src="assets/img/user/user16.jpg" alt="Img" />
-                </a>
-              </div>
-            </div>
-            <div class="profile-group">
-              <div class="profile-name text-center">
-                <h4>
-                  <a href="student-profile.html">Rolands Richard</a>
-                </h4>
-                <p>Student</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="settings-widget account-settings">
-          <div className="settings-menu">
-            <h3>Dashboard</h3>
-            <ul>
-              <li className="nav-item active">
-                <Link to="/student-dashboard" className="nav-link">
-                  <i className="bx bxs-tachometer"></i>Dashboard
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-profile" className="nav-link">
-                  <i className="bx bxs-user"></i>My Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-courses" className="nav-link">
-                  <i className="bx bxs-graduation"></i>Enrolled Courses
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-wishlist" className="nav-link">
-                  <i className="bx bxs-heart"></i>Wishlist
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-reviews" className="nav-link">
-                  <i className="bx bxs-star"></i>Reviews
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-quiz" className="nav-link">
-                  <i className="bx bxs-shapes"></i>My Quiz Attempts
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-order-history" className="nav-link">
-                  <i className="bx bxs-cart"></i>Order History
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-qa" className="nav-link">
-                  <i className="bx bxs-bookmark-alt"></i>Question & Answer
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-referral" className="nav-link">
-                  <i className="bx bxs-user-plus"></i>Referrals
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-messages" className="nav-link">
-                  <i className="bx bxs-chat"></i>Messages
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/student-tickets" className="nav-link">
-                  <i className="bx bxs-coupon"></i>Support Tickets
-                </Link>
-              </li>
-            </ul>
-            <h3>Account Settings</h3>
-            <ul>
-              <li className="nav-item">
-                <Link to="/student-settings" className="nav-link">
-                  <i className="bx bxs-cog"></i>Settings
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  <i className="bx bxs-log-out"></i>Logout
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      {/* <!-- /Sidebar --> */}
-      <Dashboard />
-      </div>
+      {/* Routing Start */}
+      
     </>
   );
 };
---
+++ client/src/pages/student/home/StudentProfile.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const StudentProfile = () => {
+  return (
+    <>
+        <div class="breadcrumb-bar breadcrumb-bar-info">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 col-12">
+							<div class="breadcrumb-list">
+								<h2 class="breadcrumb-title">My Profile</h2>
+								<nav aria-label="breadcrumb" class="page-breadcrumb">
+									<ol class="breadcrumb">
+										<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+										<li class="breadcrumb-item active" aria-current="page">My Profile</li>
+									</ol>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+            {/* <div class="page-content">
+				<div class="container">
+					<div class="row"> */}
+						{/* <!-- Student Profile --> */}
+						<div class="col-xl-9 col-lg-9">	
+
+							<div class="settings-widget card-details mb-0">
+								<div class="settings-menu p-0">
+									<div class="profile-heading">
+										<h3>My Profile</h3>
+									</div>
+									<div class="checkout-form personal-address">
+										<div class="row">
+											<div class="col-sm-6">
+												<div class="contact-info">
+													<h6>First Name</h6>
+													<p>Ronald</p>
+												</div>
+											</div>
+											<div class="col-sm-6">
+												<div class="contact-info">
+													<h6>Last Name</h6>
+													<p>Richard</p>
+												</div>
+											</div>
+											<div class="col-sm-6">
+												<div class="contact-info">
+													<h6>User Name</h6>
+													<p>studentdemo</p>
+												</div>
+											</div>
+											<div class="col-sm-6">
+												<div class="contact-info">
+													<h6>Email</h6>
+													<p><a href="https://dreamslms.dreamstechnologies.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="dba8afaebfbeb5afbfbeb6b49bbea3bab6abb7bef5b8b4b6">[email&#160;protected]</a></p>
+												</div>
+											</div>
+											<div class="col-sm-6">
+												<div class="contact-info">
+													<h6>Phone Number</h6>
+													<p>90154-91036</p>
+												</div>
+											</div>
+											<div class="col-sm-12">
+												<div class="contact-info mb-0">
+													<h6>Bio</h6>
+													<p>Hello! I'm Ronald Richard. I'm passionate about developing innovative software solutions, analyzing classic literature. I aspire to become a software developer, work as an editor. In my free time, I enjoy coding, reading, hiking etc.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>	
+						{/* <!-- Student Profile --> */}
+						
+					{/* </div>
+				</div>
+			</div>	 */}
+    </>
+  )
+}
+
+export default StudentProfile
